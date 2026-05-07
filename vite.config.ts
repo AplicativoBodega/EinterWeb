86faefb84f024bed1231236cf8a16af30deb32d2
SantiQ <a01571222@itesm.mx>
2026-05-07 16:50:17 -0600

    pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Ensures assets work with GitHub Pages
+  base: '/EinterWeb/',
   server: {
     proxy: {
       '/api': {
