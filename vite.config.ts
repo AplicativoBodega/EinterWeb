da996d66d5735e97dc87648fa16f5c0b6277400e
SantiQ <a01571222@itesm.mx>
2026-06-08 21:02:03 -0600

    updte to railway

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://einterapi-production.up.railway.app/',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '/api'),
       },
